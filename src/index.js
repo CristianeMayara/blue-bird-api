791055f2cfdc6d9e3d66e3b81abedc2161f00f9d
Cristiane Mayara <cris.mayara@gmail.com>
2019-02-10 16:48:42 -0200

    Connection with mongoDB

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+/*mongoose.connect('mongodb+srv://admin:admin123@goweek-backend-j0i7g.mongodb.net/test?retryWrites=true', {
+  useNewUrlParser: true
+});*/
 
 app.get('/', (req, res) => {
   return res.send('Hello world');
